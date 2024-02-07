b4c3751e07e9a6d6d9858bba20caf4eef81a56e2
devCarlosSantoss <carlosvinicius357890@gmail.com>
2024-02-07 20:50:15 -0300

    Config api add

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,53 +1,132 @@
+import { useState } from "react";
 import InputForm from "../InputForm/InputForm";
 import "./Formulario.css";
-import Section from "../sessao/section";
+import axios from "axios";
 
 const FormularioCadastro = () => {
-  const signup = async () => { };
+  
+  const [formData, setFormData] = useState({
+    username: null,
+    name: null,
+    email: null,
+    password: null,
+    phone: null,
+    office: null,
+  });
+
+  const handleChange = async (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
+
+  const signup = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await axios.post("http://localhost:8081/api/users/signup", formData, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      })
+      if (response.data.success || response.status == 200) {
+        // O cadastro foi bem-sucedido, você pode redirecionar o usuário ou realizar outras ações necessárias.
+        console.log("Cadastro bem-sucedido!");
+      } else {
+        // Tratar erro aqui, por exemplo, exibir uma mensagem de erro para o usuário.
+        console.error("Falha no cadastro");
+      }
+    } catch (error) {
+      console.error("Erro ao fazer cadastro", error);
+    }
+
+    limparForm();
+  };
+
+  const limparForm = () => {
+    setFormData({
+      username: "",
+      name: "",
+      email: "",
+      password: "",
+      phone: "",
+      office: "",
+    })
+  }
+
   return (
-    <>
-      <section className="form-section">
-        <form onSubmit={signup}>
-          <h1>Cadastre-se</h1>
-          <div>
-            <InputForm
-              Id="username"
-              forId="username"
-              tipo="text"
-              placeholder="Digite seu nome de usuário"
-              obrigatorio
-            >Nome de Usuário:</InputForm>
-            <InputForm
-              valor="Nome Completo:"
-              Id="nome"
-              forId="nome"
-              tipo="text"
-              placeholder="Digite o seu nome completo"
-              obrigatorio
-            >Nome Completo</InputForm>
-            <InputForm
-              Id="email"
-              forId="email"
-              tipo="email"
-              placeholder="Digite seu email:"
-              obrigatorio
-            >Email:</InputForm>
-            <InputForm
-              valor="Senha:"
-              Id="senha"
-              forId="senha"
-              tipo="password"
-              length="8"
-              placeholder="Digite sua senha"
-              obrigatorio
-            >Senha:</InputForm>
-            <InputForm placeholder="Digite seu telefone" type="tel" maxLength="11" obrigatorio>
-              Telefone:</InputForm>
-          </div>
-        </form>
-      </section>
-      <Section></Section>
-    </>
+    <section className="form-section">
+      <form onSubmit={signup} className="d-flex flex-column form-container">
+        <h1>Cadastre-se</h1>
+        <div>
+          <InputForm
+            Id="username"
+            forId="username"
+            tipo="text"
+            obrigatorio
+            value={formData.username}
+            onChange={handleChange}
+          >
+            Nome de Usuário:
+          </InputForm>
+          <InputForm
+            Id="name"
+            forId="name"
+            tipo="text"
+            obrigatorio
+            value={formData.name}
+            onChange={handleChange}
+          >
+            Nome Completo:
+          </InputForm>
+          <InputForm
+            Id="email"
+            forId="email"
+            tipo="email"
+            obrigatorio
+            value={formData.email}
+            onChange={handleChange}
+          >
+            Email:
+          </InputForm>
+          <InputForm
+            Id="password"
+            forId="password"
+            tipo="password"
+            length="8"
+            obrigatorio
+            value={formData.password}
+            onChange={handleChange}
+          >
+            Senha:
+          </InputForm>
+          <InputForm
+            Id="phone"
+            forId="phone"
+            type="tel"
+            maxLength="11"
+            obrigatorio
+            value={formData.phone}
+            onChange={handleChange}
+          >
+            Telefone:
+          </InputForm>
+          <InputForm
+            Id="office"
+            forId="office"
+            type="text"
+            obrigatorio
+            value={formData.office}
+            onChange={handleChange}
+          >
+            Função:
+          </InputForm>
+        </div>
+        <div className="btn-container">
+          <button className="btn btn-primary btn-signup">Cadastrar</button>
+        </div>
+      </form>
+    </section>
   );
 };
 
--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -5,7 +5,17 @@ const InputForm = (props) => {
   return (
     <div className="inputForm-container">
       <label htmlFor={props.forId}>{props.children}</label>
-      <input type={props.tipo} minLength={props.length} maxLength={props.maxLength} required={props.obrigatorio} id={props.Id} placeholder={props.placeholder}/>
+      <input
+        className="form-control"
+        type={props.tipo}
+        minLength={props.length}
+        maxLength={props.maxLength}
+        value={props.value}
+        required={props.obrigatorio}
+        id={props.Id}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+      />
     </div>
   );
 };
